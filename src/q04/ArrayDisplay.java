c29a6e36fcb4dd9927a61f08aacd0f9fb9f12e57
Noa0517 <n-fukaya@seassist.co.jp>
2025-05-02 10:09:26 +0900

    3週目基本文法課題

---
+++ src/q04/ArrayDisplay.java
@@ -0,0 +1,11 @@
+package q04;
+
+public class ArrayDisplay {
+	public static void main(String[] args) {
+	String name[] = {"りんご","ばなな","みかん","ぶどう","すいか"};
+	
+	for (String str: name) {
+		System.out.println(str);
+	}
+	}
+}
